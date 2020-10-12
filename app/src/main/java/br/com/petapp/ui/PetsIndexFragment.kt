3d98ad2cb78d734c3deb798f58151c034ef7a2f2
Rafael Alberto <rafael.alberto1703@gmail.com>
2020-10-12 09:41:10 -0300

    fixing layout and adding list item divider

--- app/src/main/java/br/com/petapp/ui/PetsIndexFragment.kt
+++ app/src/main/java/br/com/petapp/ui/PetsIndexFragment.kt
@@ -4,25 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
 import br.com.petapp.R
-import br.com.petapp.databinding.FragmentPetsBinding
+import br.com.petapp.databinding.FragmentPetsIndexBinding
 import br.com.petapp.viewmodel.PetViewModel
 
-class PetsFragment : Fragment() {
+class PetsIndexFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding: FragmentPetsBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_pets, container, false)
+        val binding: FragmentPetsIndexBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_pets_index, container, false)
         binding.lifecycleOwner = this
 
         val petViewModel: PetViewModel = ViewModelProviders.of(this).get(PetViewModel::class.java)
         binding.petViewModel = petViewModel
+
+        binding.petsList.apply {
+            setHasFixedSize(true)
+            val itemDecoration = DividerItemDecoration(this.context, DividerItemDecoration.VERTICAL)
+            itemDecoration.setDrawable(ContextCompat.getDrawable(this.context, R.drawable.list_item_divider)!!)
+            addItemDecoration(itemDecoration)
+        }
 
         val adapter = PetAdapter()
         binding.petsList.adapter = adapter
