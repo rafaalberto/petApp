aea2ec4493db72078a1b096b73b70d91c9578259
Rafael Alberto <rafael.alberto1703@gmail.com>
2021-02-02 14:00:11 -0300

    add back button

--- app/src/main/java/br/com/petapp/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/petapp/ui/activity/MainActivity.kt
@@ -2,6 +2,8 @@ package br.com.petapp.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import br.com.petapp.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        NavigationUI.setupActionBarWithNavController(this, findNavController())
     }
+
+    override fun onSupportNavigateUp() = findNavController().navigateUp()
+
+    private fun findNavController() = this.findNavController(R.id.my_nav_host_fragment)
 
 }
